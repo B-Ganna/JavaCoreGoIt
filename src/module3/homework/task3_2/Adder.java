5f13aba7ca467516ebef0a9d94c3ee1bcf55d596
B-Ganna <anna_bredun@mail.ru>
2017-02-01 16:03:40 +0200

    module 3, homework 3.2, added

---
+++ src/module3/homework/task3_2/Adder.java
@@ -0,0 +1,12 @@
+package module3.homework.task3_2;
+
+
+public class Adder extends Arichmetic {
+    public boolean check(Integer a, Integer b) {
+        if (a >= b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- src/module3/homework/task3_2/Arichmetic.java
+++ src/module3/homework/task3_2/Arichmetic.java
@@ -2,4 +2,7 @@ package module3.homework.task3_2;
 
 
 public class Arichmetic {
+    public Integer add(Integer a, Integer b) {
+        return a + b;
+    }
 }
