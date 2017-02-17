67a5eaba03cbab63d019f97f3d0d9214a7a93d22
B-Ganna <anna_bredun@mail.ru>
2017-02-17 13:57:12 +0200

    module 4, homework 4.5, added

--- src/module4/BankSystemImpl.java
+++ src/module4/BankSystemImpl.java
@@ -2,4 +2,23 @@ package module4;
 
 
 public class BankSystemImpl implements BankSystem {
+    @Override
+    public void withdrawOfUser(User user, int amount) {
+
+    }
+
+    @Override
+    public void fundUser(User user, int amount) {
+
+    }
+
+    @Override
+    public void transferMoney(User fromUser, User toUser, int amount) {
+
+    }
+
+    @Override
+    public void paySalary(User user) {
+
+    }
 }
