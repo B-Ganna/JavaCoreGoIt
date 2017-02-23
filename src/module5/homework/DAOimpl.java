49822499e996575546831047b8ba1a60edff0a47
B-Ganna <anna_bredun@mail.ru>
2017-02-23 13:20:11 +0200

    module 5, homework 5.5, added DAO and it's implementation

--- src/module5/homework/DAOimpl.java
+++ src/module5/homework/DAOimpl.java
@@ -4,29 +4,26 @@ package module5.homework;
 public class DAOimpl implements DAO {
     @Override
     public Room save(Room room) {
-        return null;
+        System.out.println(room + " is saved");
+        return room;
     }
 
     @Override
     public boolean delete(Room room) {
-        return false;
+        System.out.println(room + " is deleted");
+        return true;
     }
 
     @Override
     public Room upDate(Room room) {
-        return null;
+        System.out.println(room + " is updated");
+        return room;
     }
 
     @Override
     public Room findById(long id) {
         Room room = new Room(id, 0, 0, null, null, null);
-        System.out.println("");
-
+        System.out.println(room + " is found");
         return room;
-    }
-
-    @Override
-    public Room[] getAll() {
-        return new Room[5];
     }
 }
